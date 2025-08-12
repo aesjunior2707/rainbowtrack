7e86c428b63458f61155a904bc4d14410e89a913
Alexandre Junior <alesunchin@gmail.com>
2025-08-12 11:54:22 -0300

    teste

--- plugins/auth.client.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtPlugin(async () => {
-  const { $pinia } = useNuxtApp()
-  const authStore = useAuthStore($pinia)
-  
-  // Initialize auth state on client hydration
-  await authStore.checkAuth()
-})
--- plugins/translation.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(() => {
-  const { $pinia } = useNuxtApp()
-  const translationStore = useTranslationStore($pinia)
-  
-  // Initialize locale once on client side
-  if (process.client) {
-    translationStore.initLocale()
-  }
-})
